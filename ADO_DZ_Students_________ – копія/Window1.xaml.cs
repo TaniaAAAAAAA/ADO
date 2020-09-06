875f224e72e54565e82cc545e95cf9196155f424
TaniaAAAAAAA <tsntania@gmail.com>
2020-09-06 18:35:45 +0300

    Entity framework

---
+++ ADO_DZ_Students_________ – копія/Window1.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ADO_DZ_Students_________
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+         string connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+            List<int?> list_IdGroup = new List<int?>();
+        public Window1()
+        {
+            InitializeComponent();
+
+            ReadGropsFromDB();
+        }
+
+        private void ReadGropsFromDB()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+
+                string sql = "SELECT IdGroup FROM Student";
+                SqlCommand sqlCommand = new SqlCommand(sql, connection);
+
+                var reader = sqlCommand.BeginExecuteReader(CallBack, null);
+                //if (reader.HasRows)
+                //{
+                //    while (reader.Read())
+                //    {
+                //        if (!list_IdGroup.Exists(x => x == reader["IdGroup"] as Nullable<int>))
+                //            list_IdGroup.Add(reader["IdGroup"] as Nullable<int>);
+
+                //    }
+                //}
+            //    reader.Close();
+
+
+                cb_IdGroup.ItemsSource = list_IdGroup;
+
+            }
+        }
+
+        private void CallBack(IAsyncResult ar)
+        {
+            AsyncResult res = (AsyncResult)ar;
+            var result = (SqlCommand)res.AsyncDelegate;
+            var reader = result.EndExecuteReader(ar);
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    if (!list_IdGroup.Exists(x => x == reader["IdGroup"] as Nullable<int>))
+                        list_IdGroup.Add(reader["IdGroup"] as Nullable<int>);
+
+                }
+            }
+            reader.Close();
+        }
+
+        public Window1(ListBox lb, ObservableCollection<Student> students)
+        {
+            InitializeComponent();
+            ReadGropsFromDB();
+            index = lb.SelectedIndex;
+             ID = students[index].Id;
+
+            txtName.Text = students[index].Name;
+            txtSurname.Text = students[index].Surname;
+            cb_IdGroup.SelectedItem = students[index].IdGroup;
+            btnAdd.IsEnabled = false;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Student newStudent = new Student()
+            {
+                Name = txtName.Text,
+                Surname = txtSurname.Text,
+                IdGroup = Convert.ToInt32(cb_IdGroup.Text)
+            };
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString))
+            {
+                connection.Open();
+
+                AddStudent(newStudent, connection);
+            }
+            this.Close();
+        }
+
+
+
+
+        private void AddStudent(Student student, SqlConnection conn)
+        {
+            string cmdText = @"insert Student values (@name, @surname, @idGroup)";
+            SqlCommand command = new SqlCommand(cmdText, conn);
+
+        
+            command.Parameters.Add("@name", System.Data.SqlDbType.NVarChar).Value = student.Name;
+
+            command.Parameters.Add("@surname", System.Data.SqlDbType.NVarChar).Value = student.Surname;
+            command.Parameters.Add("@idGroup", System.Data.SqlDbType.Int).Value = student.IdGroup;
+           /* int rows = */
+            command.BeginExecuteNonQuery();
+
+            //if (rows > 0)
+            //{
+            //    
+            //}
+        }
+
+       
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           UpdateStudent();
+            this.Close();
+        }
+
+        int index ;
+        int ID ;
+        public void UpdateStudent()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string cmdText = $@"UPDATE Student SET Name=@name, Surname=@surname, IdGroup=@idGroup WHERE Id={ID}";
+
+
+                SqlCommand command = new SqlCommand(cmdText, conn);
+
+                command.Parameters.Add("@name", System.Data.SqlDbType.NVarChar).Value = txtName.Text;
+
+                command.Parameters.Add("@surname", System.Data.SqlDbType.NVarChar).Value = txtSurname.Text;
+                command.Parameters.Add("@idGroup", System.Data.SqlDbType.Int).Value =cb_IdGroup.SelectedItem;
+
+
+                conn.Open();
+
+                /*int rows = */
+                command.BeginExecuteNonQuery();
+            }
+        }
+
+
+    }
+}
---
+++ ADO_Entity/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            UniversityEntities context =new UniversityEntities();
+            foreach (var item in context.Student)
+            {
+                Console.WriteLine($"{item.Name}  {item.Surname}  ===>> {item.Groups.Name}");
+            }
+
+            //Console.WriteLine("Enter name student : ");
+            //string name = Console.ReadLine();
+
+            //foreach (var item in context.Student)
+            //{
+            //    if (item.Name == name)
+            //    {
+            //        context.Student.Remove(item);
+            //    }
+            //}
+
+
+            //context.SaveChanges();
+            //foreach (var item in context.Student)
+            //{
+            //    Console.WriteLine($"{item.Name}  {item.Surname}  ===>> {item.Groups.Name}");
+            //}
+
+            //-----------------------Update-------------------------
+
+            //    Console.WriteLine("Enter name group : ");
+            //    string num = Console.ReadLine();
+
+            //    foreach (var item in context.Student)
+            //    {
+            //        if (item.Name == "Alina")
+            //        {
+            //            item.Groups.Name=num;
+            //            break;
+            //        }
+            //    }
+
+            //    context.SaveChanges();
+            //    foreach (var item in context.Student)
+            //    {
+            //        Console.WriteLine($"{item.Name}  {item.Surname}  ===>> {item.Groups.Name}");
+            //    }
+
+
+
+
+            //---------------------   if (item.Groups.Name == "B123")-------------------
+
+
+            // Console.WriteLine("----------------if (item.Groups.Name == 'B123')----------------");
+            // foreach (var item in context.Student)
+            // {
+            //     if (item.Groups.Name == "B123")
+            //     {
+            //         Console.WriteLine($"{item.Name}  {item.Surname}  ===>> {item.Groups.Name}");
+            //     }
+            // }
+
+
+
+            // Console.WriteLine("----------------if (item.Groups.Name == 'B123'|| item.Groups.Name == 'Pr456')----------------");
+            // int num = 0;
+            // foreach (var item in context.Student)
+            // {
+            //     if (item.Groups.Name == "B123"|| item.Groups.Name == "Pr456")
+            //     {
+            //         num++;
+            //     }
+            // }
+            // Console.WriteLine($"Count students : {num}");
+
+
+
+            // Console.WriteLine("--------------------------Max mark C++  ----------");
+            // var mark = context.Achievement.Where(x => x.Subject.Name == "C++").Select(x => x.Mark).Max();
+
+            // var res = context.Achievement.Where(x => x.Subject.Name == "C++").Where(x => x.Mark == mark).FirstOrDefault().Student;
+
+            // Console.WriteLine(mark);
+            //Console.WriteLine($"{res.Name} {res.Surname}");
+
+
+            //Console.WriteLine("-------------------Вивести всі предмети, які читає Андрій Трофімчук------- ----------");
+
+            //var id_department = context.Teacher.Where(x => x.Name == "Andrii" && x.Surname == "Trofimchuk").Select(x => x.Id_Department).FirstOrDefault();
+            //var subjects = context.Subject.Where(x => x.Id_Department == id_department);
+            //foreach (var item in subjects)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+
+
+            //Console.WriteLine("----------------- Students with Name Olia --------------------------------------------");
+
+            //var studentsWithNameOlia = context.Student.Where(x => x.Name == "Olia").Count();
+            //Console.WriteLine($"Count tudents with Name Olia {studentsWithNameOlia}");
+
+
+
+
+
+
+
+
+            Console.WriteLine("-------------------------Студенту з мінімальною оцінкою по предмету С# змінити прізвище----------");
+            var mark = context.Achievement.Where(x => x.Subject.Name == "C#").Select(x => x.Mark).Min();
+
+            var res = context.Achievement.Where(x => x.Subject.Name == "C#").Where(x => x.Mark == mark).FirstOrDefault().Student;
+            res.Surname = "NewSurnameYessss";
+            context.SaveChanges();
+
+            Console.WriteLine(mark);
+            Console.WriteLine($"{res.Name} {res.Surname}");
+
+        }
+    }
+}
