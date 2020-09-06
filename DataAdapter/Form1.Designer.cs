e0f497222c476dc346eab4bad14ecea29cd13241
TaniaAAAAAAA <tsntania@gmail.com>
2020-09-06 19:38:23 +0300

    DataAdapter

---
+++ DataAdapter/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace DataAdapter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnLoadPic = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(784, 323);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id :";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(65, 13);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 22);
+            this.txtId.TabIndex = 2;
+            // 
+            // btnLoadPic
+            // 
+            this.btnLoadPic.Location = new System.Drawing.Point(220, 11);
+            this.btnLoadPic.Name = "btnLoadPic";
+            this.btnLoadPic.Size = new System.Drawing.Size(142, 30);
+            this.btnLoadPic.TabIndex = 3;
+            this.btnLoadPic.Text = "Load Picture";
+            this.btnLoadPic.UseVisualStyleBackColor = true;
+            this.btnLoadPic.Click += new System.EventHandler(this.btnLoadPic_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(634, 389);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(157, 49);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save All";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoadPic);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnLoadPic;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ DataAdapter/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataAdapter
+{
+
+    public partial class Form1 : Form
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+        SqlConnection connection = null;
+        SqlDataAdapter adapter = null;
+        SqlCommandBuilder builder = null;
+        DataSet set = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            connection = new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter("select * FROM Books", connection);
+            builder = new SqlCommandBuilder(adapter);
+
+            set = new DataSet();
+                adapter.Fill(set);
+           
+            dataGridView1.DataSource = set.Tables[0];
+        }
+
+        private void btnLoadPic_Click(object sender, EventArgs e)
+        {
+           
+            //    Image img = Image.FromFile(fileName);
+            //    int w = 200, h = 200;
+
+            //    Image newImg = new Bitmap(w, h);
+
+            //    Graphics g = Graphics.FromImage(newImg);
+            //    g.DrawImage(img, 0, 0, w, h);
+
+            //    newImg.Save("temp.jpg");
+
+            //}
+            //--------------------------------------------------------------
+
+         
+            byte[] image;
+            OpenFileDialog dialog = new OpenFileDialog();
+            string fileName = "";
+           
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = dialog.FileName.ToString();
+                    using (FileStream fs = new FileStream(fileName, FileMode.Open))
+                    {
+                        image = new byte[fs.Length];
+                        fs.Read(image, 0, image.Length);
+                    }
+
+                    set.Tables[0].Rows[int.Parse(txtId.Text)-1]["Picture"] = image;
+
+                    SqlCommand command = new SqlCommand($"Update Books SET Picture = @p1  where Id = @p2");
+
+                    command.Parameters.AddWithValue("@p1", image);
+                    command.Parameters.AddWithValue("@p2", int.Parse(txtId.Text));
+
+                    adapter.UpdateCommand = command;
+                    adapter.Update(set);
+
+                }
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            adapter.Update(set);
+            this.Close();
+        }
+    }
+}
